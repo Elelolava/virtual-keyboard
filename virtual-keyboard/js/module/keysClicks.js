6a848c7b0a7ed2e5211d38ac79978987e394b410
Elelolava <125636205+Elelolava@users.noreply.github.com>
2023-04-30 18:27:47 +0300

    feat: add backspace work

--- virtual-keyboard/js/module/keysClicks.js
+++ virtual-keyboard/js/module/keysClicks.js
@@ -2,26 +2,27 @@ const keys = document.querySelectorAll('.key');
 const textarea = document.querySelector('.textarea');
 
 
-const tabClicks = () => {
-  let textAreaContent = textarea.value;
-  let position = textarea.selectionStart;
-  textarea.value = textAreaContent.slice(0, position) + '\t' + textAreaContent.slice(position);
+const cursorPosition = (n) => {
+  textarea.setRangeText(n, textarea.selectionStart, textarea.selectionEnd, "end");
+  textarea.focus();
 };
 
+const tabClicks = () => {
+  let value = '\t';
+  cursorPosition(value);
+};
 
 textarea.addEventListener('keydown', (e) => {
   let keyCode = e.code.toLowerCase();
-
   keys.forEach(key => {
     key.classList.remove('key-active');
     let keyClass = key.className.slice(4).toLowerCase();
-
-    if (keyCode == keyClass && keyCode !== 'tab') {
+    if (keyCode == keyClass) {
+      if (keyCode == 'tab') {
+        e.preventDefault();
+        tabClicks();
+      }
       key.classList.add('key-active');
-
-    } else if (keyCode == keyClass && keyCode == 'tab') {
-      e.preventDefault();
-      tabClicks();
     }
   });
   setTimeout(() => {
@@ -29,21 +30,26 @@ textarea.addEventListener('keydown', (e) => {
   }, 500);
 });
 
-
 keys.forEach(key => {
   key.addEventListener('click', (e) => {
     e.preventDefault();
+    textarea.focus();
     let textAreaContent = textarea.value;
     let keyText = key.children[1].children[0].textContent;
 
     if (keyText == 'Backspace') {
-      let textAreaCutContent = textAreaContent.slice(0, -1);
+      let cutString = textarea.selectionStart == textarea.selectionEnd ? 
+        textAreaContent.slice(textarea.selectionStart-1, textarea.selectionEnd) : 
+        textAreaContent.slice(textarea.selectionStart, textarea.selectionEnd);
+      let textAreaCutContent = textAreaContent.replace(cutString, '');
+      console.log(textarea.selectionStart, textarea.selectionEnd, cutString, textAreaCutContent);
       textarea.value = textAreaCutContent;
 
     } else if (keyText == 'Tab') {
       tabClicks();
 
     } else if (keyText == 'Del') {
+
     } else if (keyText == 'CapsLock') {
     } else if (keyText == 'Enter') {
     } else if (keyText == 'Shift') {
@@ -51,8 +57,8 @@ keys.forEach(key => {
     } else if (keyText == 'Alt') {
     } else if (keyText == 'Win') {
     } else {
+      cursorPosition(keyText);
       textAreaContent += keyText;
-      textarea.value = textAreaContent;
     }
   });
 });
